c53d954d1a76ab480e33cdf20b54e7d9c8ac2285
Veeramallu Bhanu Sai <132256826+Vbhanusai@users.noreply.github.com>
2025-03-17 12:13:14 +0530

    Create file

---
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        # Take two pointers low and high
+        low,high = 0 ,len(nums)-1
+        ans = float('inf')
+        while low<=high:
+            # This range is completely sorted
+            # low element is smallest element
+            if nums[low]<=nums[high]:
+                ans = min(ans,nums[low])
+                return ans
+            mid = (low+high)//2
+
+            # if low element is less than mid element
+            # search the right half
+            if nums[low]<=nums[mid]:
+                ans = min(ans,nums[low])
+                low = mid + 1
+
+            # if low element is less than mid element
+            # search the right half
+            else:
+                ans = min(ans,nums[mid])
+                high = mid - 1
+        return ans
