b707c9c193be76aebc8d8f96712ed9d8ff7ab8c1
Veeramallu Bhanu Sai <132256826+Vbhanusai@users.noreply.github.com>
2025-03-17 10:22:00 +0530

    Create file

---
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        # initialize prefix, suffix and ans
+        prefix = 1
+        suffix = 1
+        ans = -float('inf')
+
+        #iterate over nums
+        for i in range(len(nums)):
+
+            #if prefix or suffix product is zero don't multiply with further numbers
+            if prefix==0:
+                prefix = 1
+            if suffix==0:
+                suffix = 1
+            
+            #calculate prefix and suffix
+            prefix = prefix*nums[i]
+            suffix = suffix*nums[len(nums)-1 - i]
+
+            #find max product so far
+            ans = max(ans,max(prefix,suffix))
+            
+        return ans
